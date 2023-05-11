4025f9aa40df93d2f62846930af0d439261bc113
KaKeGo <funfcia@hotmail.com>
2023-05-11 22:05:03 +0200

    create first models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    def create_user(self):
+        pass
+    def create_superuser(self):
+        pass
+
+class User(AbstractUser):
+    pass
