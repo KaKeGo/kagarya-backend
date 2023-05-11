0986cc2921f304e7851070f63f699a3e76403890
KaKeGo <funfcia@hotmail.com>
2023-05-11 22:44:02 +0200

    Some fixes to work

--- users/models.py
+++ users/models.py
@@ -7,21 +7,40 @@ from django.contrib.auth.models import AbstractUser
 
 
 class UserManager(BaseUserManager):
-    def create_user(self):
-        pass
-    def create_superuser(self):
-        pass
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('Email must be set for user')
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save()
+        return user
+            
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        return self.create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
-    username = models.CharField(max_length='40')
-    email = models.EmailField(max_length='30', unique=True)
-    is_staff = models.BooleanField(default=False)
+    username = models.CharField(max_length=40)
+    email = models.EmailField(max_length=30, unique=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=True)
     is_active = models.BooleanField(default=True)
     date_created = models.DateTimeField(default=timezone.now)
     last_login = models.DateField(auto_now=True)
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
+    REQUIRED_FIELDS = []
     
     objects = UserManager()
     
