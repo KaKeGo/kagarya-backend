ceaa52384660a5f379270d31c6871d284a147a01
KaKeGo <funfcia@hotmail.com>
2023-05-12 21:38:41 +0200

    Create form and first admin model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import (
+    User,
+)
+from .forms import (
+    CustomUserChangeForm,
+    CustomUserCreationForm,
+)
 
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    
+    model = User
+    
+    list_display = ('username', 'email', 'is_active')
+
+
+admin.site.register(User, CustomUserAdmin)
