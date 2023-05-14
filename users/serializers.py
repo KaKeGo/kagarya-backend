d8edb9a820b136404665a41802ad15022efe91a8
KaKeGo <funfcia@hotmail.com>
2023-05-14 16:38:58 +0200

    Add view for validate username

--- users/serializers.py
+++ users/serializers.py
@@ -16,19 +16,52 @@ class UserCreateSerializer(serializers.ModelSerializer):
             'password': {'write_only': True}
         }
         
+    def validate_username(self, username):
+        if len(username) < 2 or len(username) > 30:
+            raise serializers.ValidationError({'Username': 'Username must have more then 2 character or less then 30'})
+        return username
+        
     def save(self):
         user = CustomUser(
             email=self.validated_data['email'],
-            username=self.validated_data['username']
+            username=self.validated_data['username']      
             )
+
         password = self.validated_data['password']
         password2 = self.validated_data['password2']
         if password != password2:
-            raise serializers.ValidationError({'password': 'Password must match'})
+            raise serializers.ValidationError({'Password': 'Password must match'})
+        if len(password) < 6 or len(password) > 16:
+            raise serializers.ValidationError({'Password': 'Password must be longer than 4 characters'})
+
         user.set_password(password)
         user.save()
         return user
-    
+
+        
+        # try:
+        #     if len(username) < 2:
+        #             return Response({'error': 'Username must be longer than 2 characters.'})
+        #     if password == re_password:
+        #         if CustomUser.objects.filter(email=email).exists():
+        #             return Response({'error': 'Account with this email already exists.'})
+        #         else:
+        #             if len(password) < 6:
+        #                 return Response({'error': 'Password must have at least 6 characters.'})
+        #             else:
+        #                 user = CustomUser.objects.create(
+        #                     email=email,
+        #                     username=username,
+        #                     password=password,
+        #                 )
+        #                 user.save()
+        #                 return Response({'success': 'Account created successfully.'})
+        #     else:
+        #         return Response({'error': 'Password do not match'}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        # except:
+        #     return Response({'error': 'Something went wrong with creating user try again.'}, status=status.HTTP_400_BAD_REQUEST)
+ 
+
 class PasswordChangeSerializer(serializers.Serializer):
     current_password = serializers.CharField(style={'input_type': 'password'}, required=True)
     new_password = serializers.CharField(style={'input_type': 'password'}, required=True)
--- users/views.py
+++ users/views.py
@@ -34,33 +34,13 @@ class CheckAuthenticatedView(APIView):
 class CreateUserAccount(APIView): 
     def post(self, request):
         serializer = UserCreateSerializer(data=request.data)
+        
         if serializer.is_valid():
             serializer.save()
             return Response({'success': 'Accout was created'}, status=status.HTTP_200_OK)
-        return Response({'error': 'Something went wrong with creating user'}, status=status.HTTP_400_BAD_REQUEST)
         
-        # try:
-        #     if len(username) < 2:
-        #             return Response({'error': 'Username must be longer than 2 characters.'})
-        #     if password == re_password:
-        #         if CustomUser.objects.filter(email=email).exists():
-        #             return Response({'error': 'Account with this email already exists.'})
-        #         else:
-        #             if len(password) < 6:
-        #                 return Response({'error': 'Password must have at least 6 characters.'})
-        #             else:
-        #                 user = CustomUser.objects.create(
-        #                     email=email,
-        #                     username=username,
-        #                     password=password,
-        #                 )
-        #                 user.save()
-        #                 return Response({'success': 'Account created successfully.'})
-        #     else:
-        #         return Response({'error': 'Password do not match'}, status=status.HTTP_406_NOT_ACCEPTABLE)
-        # except:
-        #     return Response({'error': 'Something went wrong with creating user try again.'}, status=status.HTTP_400_BAD_REQUEST)
- 
+        return Response({'error': 'Something went wrong with creating user'}, status=status.HTTP_400_BAD_REQUEST)
+
 class ChangePasswordView(APIView):
     permission_classes = [permissions.IsAuthenticated, ]
     
