33595e279052c2e3156e3b99ce75df1781290d42
KaKeGo <funfcia@hotmail.com>
2023-05-14 15:27:40 +0200

    Add password change serializer

--- users/serializers.py
+++ users/serializers.py
@@ -28,6 +28,15 @@ class UserCreateSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+    
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(style={'input_type': 'password'}, required=True)
+    new_password = serializers.CharField(style={'input_type': 'password'}, required=True)
+    
+    def validate_current_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError({'current_password': 'Does not match'})
+        raise value
 
 class ProfilesListSerializer(serializers.ModelSerializer):
     class Meta:
