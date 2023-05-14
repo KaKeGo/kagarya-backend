5b2d9c2df0a4831dfa18dd5897bbb36410d7f13a
KaKeGo <funfcia@hotmail.com>
2023-05-14 12:14:06 +0200

    Create serializer for list profiles add urls

---
+++ users/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from .models import (
+    CustomUser,
+    UserProfile,
+)
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'username', 'password1', 'password2') 
+
+class ProfilesListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('email',)
