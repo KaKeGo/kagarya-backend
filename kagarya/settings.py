c8748ece97fc19a101bd98e534a6834cd3809a74
KaKeGo <funfcia@hotmail.com>
2023-05-20 23:05:12 +0200

    ...

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-15!1yjr!yptuwy@f!obworkp)n@c+_lojzwpi$&_m(dgq-35o^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
