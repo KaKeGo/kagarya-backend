4487d9ccd7f1895cd6dcd23e0270b56be213c1c1
KaKeGo <funfcia@hotmail.com>
2023-05-21 01:35:54 +0200

    fix

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -24,11 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-15!1yjr!yptuwy@f!obworkp)n@c+_lojzwpi$&_m(dgq-35o^'
 
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    '*'
+    '.herokuapp.com',
+    'localhost',
+    '127.0.0.1',
   ]
 
 # Application definition
