e1452cbde8aa0c77f7b6b61bab0a9177b9592652
KaKeGo <funfcia@hotmail.com>
2023-05-21 00:38:49 +0200

    Add allowed host

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-15!1yjr!yptuwy@f!obworkp)n@c+_lojzwpi$&_m(dgq-35o^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://kagarya-back.herokuapp.com']
 
 
 # Application definition
