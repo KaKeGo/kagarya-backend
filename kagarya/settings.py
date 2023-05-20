02ab6a5aae6e8b79df1440a99925050279cb2d47
KaKeGo <funfcia@hotmail.com>
2023-05-20 22:47:09 +0200

    config

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -161,8 +161,6 @@ CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000'
     "http://127.0.0.1:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
  ]
 
 django_heroku.settings(locals())
