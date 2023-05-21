834e3fa9bac3c7f05ddb91f202d419582aaa4fdf
KaKeGo <funfcia@hotmail.com>
2023-05-21 14:46:04 +0200

    Create model for todo

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    complited = models.BooleanField(default=False)
+    date_created = models.DateTimeField(default=timezone.now())
+    slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.title
+    
