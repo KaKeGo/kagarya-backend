c4953a4bd181297f7e9f57989d313ff1fe2309a9
KaKeGo <funfcia@hotmail.com>
2023-05-21 15:18:21 +0200

    create list serializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,1 +1,13 @@
 from rest_framework import serializers
+
+from .models import (
+    ToDo,
+)
+
+
+class ToDoListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ToDo
+        fields = (
+                    'title', 'description', 'completed', 'date_created', 'slug'
+                  )
