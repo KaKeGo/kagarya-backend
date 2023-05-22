ef28137c92e6ead8e5a860d0385b816ec4dd40d1
KaKeGo <funfcia@hotmail.com>
2023-05-22 18:53:52 +0200

    test

--- kagarya/settings.py
+++ kagarya/settings.py
@@ -134,6 +134,8 @@ MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'files', 'static'), )
+
 # STORAGES = {
 #     # ...
 #     "staticfiles": {
